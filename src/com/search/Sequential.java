dd8f0fe7142236b892c7bf0c85f34952ebbb67f0
DD0s-JCN13 <jimmylin160@gmail.com>
2018-09-24 16:06:31 +0800

    set build fin

--- src/com/search/Sequential.java
+++ src/com/search/Sequential.java
@@ -1,32 +1,28 @@
 package com.search;
 
 import java.util.Scanner;
+import java.util.HashSet;
+import java.util.Iterator;
 import java.util.Random;
+import java.util.Set;
 public class Sequential {
 	//循序搜尋法
 	public static void main(String args[]) {
 		//設計一個在1~100之間隨機產生20個不排序整數數列的搜尋程式
-		int i,j;
 		int input = 0;
+		int counter = 1;
 		boolean power = true;
-		boolean check = true;
 		Random r = new Random();
-		int base[] = new int[20];
-		for(i=0;i<20;i++) {
-			base[i] = r.nextInt(100)+1;
-		}
+		Set list = new HashSet();
+		do {
+			list.add(r.nextInt(100)+1);
+		}while(list.size()<20);
+		Iterator it = list.iterator();
 		Scanner sc = new Scanner(System.in);
-		while(power) {
-			j=base.length;
-			for(i=0;i<j;i++) {
-				System.out.println("第"+(i+1)+"個數字：["+base[i]+"]");
-				while(i==j-1) {
-					power = false;
-					break;
-				}
-			}
+		while(it.hasNext()) {
+			System.out.println("第"+(counter)+"個數字：["+it.next().toString()+"]");
+			counter = counter + 1;
 		}
-		
 	}
 
 }
