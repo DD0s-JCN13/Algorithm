646d1488a5c2e11257ad3a63b9e7393e5990a36f
DD0s-JCN13 <jimmylin160@gmail.com>
2018-09-29 15:35:01 +0800

    Selection sort fin

---
+++ src/com/sort/Selection.java
@@ -0,0 +1,48 @@
+package com.sort;
+
+public class Selection {
+	//選擇排序法 Selection sort
+	static int list[] = new int[] {19,53,26,12,5,64,9};
+	static int tmp = 0;
+	public void Selection() {
+		
+	}
+	public void origin() {
+		System.out.print("初始資料結構：");
+		for(int i=0;i<list.length;i++) {
+			System.out.print(list[i]+" ");
+		}
+		System.out.println("");
+		System.out.println("------------------------------");
+	}
+	public void exchange() {
+		for(int i=0;i<list.length-1;i++) {
+			for(int j=i+1;j<list.length;j++) {
+				if(list[i]>list[j]) {
+					tmp = list[i];
+					list[i] = list[j];
+					list[j] = tmp;
+				}
+			}
+			System.out.print("第"+(i+1)+"次執行結果：");
+			for(int k=0;k<list.length;k++) {
+				System.out.print(list[k]+" ");
+			}
+			System.out.println("");
+		}
+		System.out.println("------------------------------");
+	}
+	public void fin() {
+		System.out.print("最終執行結果： ");
+		for(int i=0;i<list.length;i++) {
+			System.out.print(list[i] + " ");
+		}
+		System.out.println("");
+	}
+	public static void main(String args[]) {
+		Selection s = new Selection();
+		s.origin();
+		s.exchange();
+		s.fin();
+	}
+}
