29d29ae8cbea5d99b81ac54aa94c2ebb78e0f9c9
DD0s-JCN13 <jimmylin160@gmail.com>
2018-09-29 14:08:15 +0800

    Binary fin

--- src/com/search/Binary.java
+++ src/com/search/Binary.java
@@ -6,16 +6,8 @@ import java.util.Scanner;
 public class Binary {
 	//二元搜尋法
 	//在1~20的整數進行二元搜尋法
-	static int bin = 0;
-	static int end = 19;
-	static int tmp = 1;
 	static int[] list = new int[20];
 	static boolean power = true;
-	
-	public static int codereturn() {
-		return tmp;
-	}
-	
 	
 	public static void main(String args[]) {
 		boolean binary = false;
@@ -25,17 +17,59 @@ public class Binary {
 		Scanner sc = new Scanner(System.in);
 		while(power) {
 			System.out.print("請輸入1~20的數字，或輸入0結束程式：");
+			int beg = 1;
+			int end = 20;
+			int count = 0;
 			int input = sc.nextInt();
 			if(input == 0) {
-				for(int i=0; i<list.length;i++) {
-					if(list[i]<10) {
-						System.out.println("第  "+(i+1)+"個數字[ "+list[i]+"]");
+				power = false;
+			}else if(input > 0 && input <= list.length) {
+				binary = true;
+				while(binary) {
+					int mid = (beg+end)/2;
+					if(input>mid) {
+						System.out.println(input + " 在["+mid+"]與["+end+"]之間，取右半邊");
+						beg = mid;
+						if(beg == 19) {
+							count++;
+						}
+						if(count>0) {
+							int tmp = 0;
+							for(int i=beg-1;i<=end-1;i++) {
+								if(list[i]==input) {
+									tmp = i+1;
+								}
+							}
+							System.out.println("在第"+tmp+"個位置找到數字["+end+" ]");
+							binary = false;
+						}
+					}else if(input<mid) {
+						System.out.println(input + " 在["+beg+"]與["+mid+"]之間，取左半邊");
+						end = mid;
+						if(end == 2) {
+							count++;
+						}
+						if(count>0) {
+							int tmp = 0;
+							for(int i=beg-1;i<=end-1;i++) {
+								if(list[i]==input) {
+									tmp = i+1;
+									}
+								}
+								System.out.println("在第"+tmp+"個位置找到數字["+beg+" ]");
+								binary = false;
+							}
 					}else {
-						System.out.println("第"+(i+1)+"個數字["+list[i]+"]");
+						int tmp = 0;
+						for(int i=beg-1;i<=end-1;i++) {
+							if(list[i]==input) {
+								tmp = i+1;
+							}
+						}
+						System.out.println(input + "即為中間值，在第"+tmp+"個位置找到數字["+mid+" ]");
+						binary = false;
 					}
 				}
-			}else if(input > 0 || input <= list.length) {
-				binary = true;
 				
 			}else {
 				System.out.println("資料格式錯誤，請重新輸入！");
