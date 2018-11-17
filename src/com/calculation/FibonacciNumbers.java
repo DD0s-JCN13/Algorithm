e2679cd2db1fc4076a81d8eae055fd71dddb9c45
DD0s-JCN13 <jimmylin160@gmail.com>
2018-11-17 15:35:13 +0800

    Fibonacci fin

---
+++ src/com/calculation/FibonacciNumbers.java
@@ -0,0 +1,37 @@
+package com.calculation;
+
+import java.util.Scanner;
+
+public class FibonacciNumbers {
+	public static void main(String args[]) {
+	
+	int Fn=0;
+	boolean power = true;
+	Scanner sc = new Scanner(System.in);
+	while(power) {
+		int tmp1=1;
+		int res=0;
+		System.out.println("請輸入要計算的費氏級數，或輸入-1結束程式：");
+		String in = sc.nextLine();
+		Fn = Integer.parseInt(in);
+		if(Fn==-1) {
+			power = false;
+			System.out.println("程式已關閉");
+			break;
+		}else {
+			for(int i=0;i<=Fn;i++) {
+				if(i>2) {
+					res=res+tmp1;
+					tmp1++;
+				}else if(i==2) {
+					res = res+tmp1;
+				}else {
+					res=1;
+				}
+			}
+			System.out.println(Fn+"的費氏級數為  "+res);
+		}
+	}	
+}
+}
+
