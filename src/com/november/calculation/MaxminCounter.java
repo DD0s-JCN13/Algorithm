8cddc37b3b48dfe06f6d2172b8cb7d06043390a2
DD0s-JCN13 <jimmylin160@gmail.com>
2018-11-24 18:04:51 +0800

    Mmc tmp2

--- src/com/november/calculation/MaxminCounter.java
+++ src/com/november/calculation/MaxminCounter.java
@@ -4,11 +4,16 @@ import java.util.List;
 import java.util.ArrayList;
 import java.util.Scanner;
 
+
 public class MaxminCounter {
 	static boolean input = true;
 	static boolean pusher = false;
-	static List<Integer> ls = new ArrayList();
-	public static void main(String args[]){
+	static int counter=0;
+	static List<Integer> ls = new ArrayList<Integer>();
+	public void MaxminCounter() {
+		
+	}
+	public void log() {
 		Scanner sc = new Scanner(System.in);
 		while(input) {
 			System.out.println("請輸入要加入的數字，或輸入x結束輸入：");
@@ -18,15 +23,35 @@ public class MaxminCounter {
 					input=false;
 					pusher=true;
 				}else {
-					ls.add(Integer.parseInt(read));
+					try {
+						ls.add(Integer.parseInt(read));
+					} catch (NumberFormatException e) {
+						// TODO Auto-generated catch block
+						System.out.println("輸入格式錯誤，請重新輸入！");
+					}
+					counter++;
 			 	}
 			}
-		
+		}
 		while(pusher) {
-			for(int i=0;i<ls.size();i++) {
-				System.out.print(ls.get(i)+" ");
+			int tmp=0;
+			for(int i=1;i<ls.size();i++) {
+				for(int j=0;j<ls.size()-1;j++) {
+					if(ls.get(j) > ls.get(j+1)) {
+						tmp = ls.get(j);
+						ls.set(j, ls.get(j+1));
+						ls.set(j+1, tmp);
+					}
+				}
 			}
+			pusher=false;
 		}
 	}
+	public void searching() {
+		System.out.println("");
+	}
+	public static void main(String args[]) {
+		MaxminCounter mmc = new MaxminCounter();
+		mmc.log();
 	}
 }
