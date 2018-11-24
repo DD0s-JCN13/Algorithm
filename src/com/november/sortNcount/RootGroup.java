8afc70b0c1a94b7db851e1bf7322a3d566acd059
DD0s-JCN13 <jimmylin160@gmail.com>
2018-11-24 21:37:07 +0800

    rg fin

--- src/com/november/sortNcount/RootGroup.java
+++ src/com/november/sortNcount/RootGroup.java
@@ -1,5 +1,42 @@
 package com.november.sortNcount;
 
 public class RootGroup {
-
+	static int[] list = {1,4,7};
+	public RootGroup() {
+		
+	}
+	public void sorter() {
+		//Single item
+		for(int i=0;i<list.length;i++) {
+			if(i==(list.length-1)) {
+				System.out.println("["+list[i]+"]");
+			}else {
+				System.out.print("["+list[i]+"]  ");
+			}
+		}
+		//Two items
+		for(int i=0;i<(list.length-1);i++) {
+			for(int j=i+1;j<list.length;j++) {
+				if(i==(list.length-2)) {
+					System.out.println("["+list[i]+","+list[j]+"]");
+				}else {
+					System.out.print("["+list[i]+","+list[j]+"]  ");
+				}
+			}
+		}
+		//All items
+		System.out.print("[");
+		for(int i=0;i<list.length;i++) {
+			if(i==list.length-1) {
+				System.out.print(list[i]);
+			} else{
+				System.out.print(list[i]+",");
+			}
+		}
+		System.out.print("]");
+	}
+	public static void main(String args[]) {
+		RootGroup rg = new RootGroup();
+		rg.sorter();
+	}
 }
