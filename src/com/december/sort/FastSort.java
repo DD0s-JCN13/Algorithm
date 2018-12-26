b7f5c104f8f5e6187c77c5e10479ea07e1645bde
DD0s-JCN13 <jimmylin160@gmail.com>
2018-12-26 23:00:57 +0800

    FastSort tmp1

---
+++ src/com/december/sort/FastSort.java
@@ -0,0 +1,43 @@
+package com.december.sort;
+
+import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class FastSort {
+	Scanner sc;
+	static List<Integer> data = new ArrayList<Integer>();
+	public FastSort() {
+		
+	}
+	public void Sorting() {
+		boolean sorter = true;
+		int counter=0;
+		int getterL=0;
+		int getterR=0;
+		while(sorter) {
+			int K = data.get(0);
+			for(int i=1;i<data.size();i++) {
+				if(data.get(i)>K) {
+					getterL = data.get(i);
+					break;
+				}
+			}
+			for(int i=data.size()-1;i>1;i--) {
+				if(data.get(i)<K) {
+					getterR = data.get(i);
+					break;
+				}
+			}
+			while(getterL!=0&&getterR!=0) {
+				
+			}
+			
+			
+			
+		}
+	}
+	
+
+}
